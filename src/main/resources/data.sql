6f257236bb717b1bdbaba86a9ebf1b3428a0ee0a
Prateek Srivastav <prateek.srivastav.7374@gmail.com>
2025-01-01 23:31:46 +0530

    updated data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into todo (id, description, done, target_date,username)
-values (10001, 'Learn AWS', false, CURRENT_DATE(), 'in28minutes');
+values (10001, 'Learn AWS', false, CURRENT_DATE(), 'Prateek');
 
 insert into todo (id, description, done, target_date,username)
-values (10002, 'Get AWS Certified', false, CURRENT_DATE(), 'in28minutes');
+values (10002, 'Get AWS Certified', false, CURRENT_DATE(), 'Prateek');
 
 insert into todo (id, description, done, target_date,username)
-values (10003, 'Learn DevOps', false, CURRENT_DATE(), 'in28minutes');
+values (10003, 'Learn DevOps', false, CURRENT_DATE(), 'Prateek');
